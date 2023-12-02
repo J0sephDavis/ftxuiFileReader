9db73a5f0df1d819c59519c8184de81b03564a1e
Joseph Davis <josephdavisjoey@gmail.com>
2023-12-02 12:30:53 -0500

    updated quit_handler(...) to properly exit the loop using the closure. Made the viewing pane a scrollable frame using the arrow keys

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,20 @@
 #include <fstream>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/component/component.hpp> //for CatchEvent()
+#include <ftxui/dom/elements.hpp>
 #include <sstream>
 #include <vector>
 #include <string>
 #include <filesystem>
 
+//TODO: scrollbar
+//TODO: line-numbers
+//TODO: allow command input, vim-style at the bottom
+//TODO: jump to line
+//TODO: syntax-highlighting
+
 namespace fs = std::filesystem;
 namespace ui = ftxui;
-
 //component to display the text & render
 class viewingPane : public ui::ComponentBase {
 	private:
@@ -37,23 +43,48 @@ int main(int argc, char** argv) {
 		while(std::getline(file, fileLine)) {
 			file_contents.push_back(fileLine);
 		}
-		file.close();
+		file.close(); //likely unnecessary as it is deconstructed after this.
 	}
 	if (file_contents.empty()) exit(EXIT_FAILURE);
 	//the screen that contains(is) the top-level component
 	auto application_screen = ui::ScreenInteractive::FitComponent();
-	//the viewing-pane to render
-	ui::Component test_comp = ui::Make<viewingPane>(std::move(file_contents));
-	//component decorator that will quit the current component loop
-	test_comp |= ui::CatchEvent([&application_screen](ui::Event event){
+	auto quit_handler = ui::CatchEvent([&application_screen](ui::Event event){
 		if (event == ui::Event::Character('q')) {
-			application_screen.ExitLoopClosure();
-			application_screen.Exit();
+			application_screen.ExitLoopClosure()();
 			return true;
 		}
 		return false; //no event handled
 	});
-	
-	application_screen.Loop(test_comp | ui::border);
+	//
+	float focus_x=0, focus_y=0;
+	ui::Component test_comp = ui::Make<viewingPane>(std::move(file_contents));
+#define increment 0.05f
+	test_comp |= ui::CatchEvent([&focus_x,&focus_y](ui::Event event){
+			if (event == ui::Event::ArrowDown) {
+				focus_y = (focus_y + increment > 1.0f ? 1.f : focus_y + increment);
+				return true;
+			}
+			if (event == ui::Event::ArrowUp) {
+				focus_y -= (focus_y - increment < 0.f ? 0.f : focus_y - increment);
+				return true;
+			}
+			if (event == ui::Event::Escape) {
+				focus_y = 0.f;
+				return true;
+			}
+			return false;
+	});
+	auto scroll_renderer = ui::Renderer(test_comp, [&] {
+			auto header = "Viewing Pane Renderer x:(" + std::to_string(focus_x) + ") y:(" + std::to_string(focus_y) + ")";
+			return ui::vbox({
+					ui::text(header),
+					ui::separator(),
+					test_comp->Render()
+						| ui::focusPositionRelative(focus_x, focus_y)
+						| ui::frame
+						| ui::vscroll_indicator,
+			}) | ui::border;
+	});
+	application_screen.Loop(scroll_renderer | quit_handler);
 	exit(EXIT_SUCCESS);
 }
